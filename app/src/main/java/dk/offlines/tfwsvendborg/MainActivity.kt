32c6a0bac0f2e2c043f281b881bbed64b61cb087
mikk620i <mikk620i@edu.ucl.dk>
2020-02-27 17:27:58 +0100

    Tilføjet login og registrer

--- app/src/main/java/dk/offlines/tfwsvendborg/MainActivity.kt
+++ app/src/main/java/dk/offlines/tfwsvendborg/MainActivity.kt
@@ -1,10 +1,12 @@
 package dk.offlines.tfwsvendborg
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,27 +15,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+
+
+        mainButtonLogin.setOnClickListener {
+            var userName = username.text.toString()
+            var password = password.text.toString()
+
+            if(userName == "Test" && password == "Test") {
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Error wrong username or password", Toast.LENGTH_LONG).show()
+            }
+
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        mainButtonRegisterNewUser.setOnClickListener {
+            val intent = Intent (this, RegisterActivity::class.java)
+            startActivity(intent)
         }
+
+
     }
+
+
 }
